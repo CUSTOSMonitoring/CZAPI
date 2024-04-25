f4c4ddde07317c37807889b40b2cf864f0dc66de
CUSTOSMonitoring <info@custos.uy>
2024-04-25 12:23:53 -0300

    Add files via upload

--- CompletarObjCreate.sh
+++ CompletarObjCreate.sh
@@ -1,60 +1,51 @@
 # Para completar los comandos de ZAPI
-# VERSION Tue Sep 20 19:40:36 -03 2022
-
-function Gus_GetListaObjetos()
-{
-   GusDebugObjetos "ObjCreate $1 --def_nom | jq -r '.[]'"
-   ObjCreate $1 --def_nom | jq -r '.[]'
-}
-
-function Gus_GetListaParamObj()
-{
-   ObjCreate $2 $1 --def_para  | jq -r ' keys[] | .'
-}
-
-function GusDebugObjetos()
-{
-   echo $(date +"*%F %T") "$*" >> Log.CompletarObjetos
-   #echo $(date +"*%F %T") $* > /dev/null
-}
+# vim: set ts=3 expandtab shiftwidth=3:
 
 function Gus_Complete_ObjCreate ()
 {
-   local cur prev Obj band OptsGral OptsFunc OptsParams
+   local cur prev Obj band OptsGral OptsFunc OptsParams i
    COMPREPLY=()
    cur="${COMP_WORDS[COMP_CWORD]}"
    prev="${COMP_WORDS[COMP_CWORD-1]}"
-   OptsGral="-c --def --def_nom --help --variables --init -t --traduccion"
+   AntPrev="${COMP_WORDS[COMP_CWORD-2]}"
+   OptsGral="-c --configuration --def --def_nom --help --variables --init -t --traduccion --lista-objetos --lista-propiedades --edit-objetos --debug-level"
 
-   if [[ ${prev} == "-c" ]]
-   then
-      dirConfCompletion=$( ObjCreate --variables=DirConn)
-      a=$( find ${dirConfCompletion} -maxdepth 1 -name '*.zapi' -type f ; find . -maxdepth 1 -name '*.zapi' -type f )
+   i=1
+   while [ $i -lt ${COMP_CWORD} ]
+   do # determinamos cual es el archivo de configuracion que vamos a usar
+      if [[ "${COMP_WORDS[i]}" == "-c" || "${COMP_WORDS[i]}" == "--configuration" ]]
+      then
+         let i++
+         [ $i -lt ${COMP_CWORD} ] && ArchConf="-c ${COMP_WORDS[i]}"
+      fi
+      let i++
+   done
+
+   if [[ "${prev}" == "-c" || "${prev}" == "--configuration" ]]
+   then # el paramentro de esta bandera es un archivo de configuracion
+      dirConfCompletion=$( $1 ${ArchConf} --variables=DirConn)
+      a=$( find ${dirConfCompletion} -name '*.zapi' -type f ; find . -maxdepth 1 -name '*.zapi' -type f )
       COMPREPLY=( $(compgen -W "$a" -- ${cur}) )
       if [ ${#COMPREPLY} -eq 0 ]
       then
          COMPREPLY=( $(compgen -W "$a" -- ${dirConfCompletion}/${cur} ) )
       fi
+   elif [[ "${prev}" == "-t" || "${prev}" == "--traduccion" || "${prev}" == "--lista-propiedades" ]]
+   then
+      COMPREPLY=( $( $1 ${ArchConf} --lista-objetos "^${cur}" ) )
+   elif [[ "${AntPrev}" == "-t" || "${AntPrev}" == "--traduccion" || "${AntPrev}" == "--lista-propiedades" ]]
+   then
+      COMPREPLY=( $( $1 ${ArchConf} --lista-propiedades ${prev} "^${cur}" ) )
+   elif [[ "${prev}" == "--lista-objetos" ]]
+   then # el parametro debe ser especificado en la linea de comandos, podríamos mañana sugerir alguna creación
+      COMPREPLY=( $( $1 ${ArchConf} --lista-objetos "${cur}" ) )
+   elif [[ "${prev}" == "--init" ]]
+   then # el parametro debe ser especificado en la linea de comandos, podríamos mañana sugerir alguna creación
+      COMPREPLY=( )
    else
-      GusDebugObjetos "No tenemos Objeto, van los objetos y opciones generales recorremos las banderas en ${COMP_WORDS[@]} desde 1 a $(( ${COMP_CWORD} -1 ))"
-      for i in $(seq 1 $(( ${COMP_CWORD} -1 )) )
-      do
-	GusDebugObjetos "Recorriendo, actual ${COMP_WORDS[$i]}"      
-        if [[ "${COMP_WORDS[$i]}" == "-c" ]]
-        then # el siguiente tiene que se el archivo de conf sino, no estariamos aca
-           BanderaConf="-c ${COMP_WORDS[$i+1]}"
-	   GusDebugObjetos "Seteamos BanderaConf='-c ${COMP_WORDS[$i+1]}'"
-        fi
-      done
-      if [ ! "${Obj}" ]
-      then # Completamos con los objetos
-         GusDebugObjetos No tenemos Objeto, van los objetos y opciones generales
-         COMPREPLY=( $(compgen -W "$(Gus_GetListaObjetos "${BanderaConf}" ) ${OptsGral}" -- ${cur}) )
-      else
-         # hay problemas con el = porque separa palabras
-         GusDebugObjetos tenemos objeto ${Obj}, van los parametros 
-         COMPREPLY=( $(compgen -W "$(Gus_GetListaParamObj ${Obj} ${BanderaConf} ) ${Optsparam}" -- ${cur}) )
-      fi
+      # si no es nada específico, es un objeto y/o las banderras
+      a=$( $1 ${ArchConf} --lista-objetos "${cur}" )
+      COMPREPLY=( $(compgen -W "$a $OptsGral" -- ${cur}) )
    fi
 }
 
